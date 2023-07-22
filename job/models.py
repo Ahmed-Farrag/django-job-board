dd4317c7e8a40b0c4756fe1cb22810c4bc1c5aef
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-07-23 00:59:59 +0300

    add job views & url

--- job/models.py
+++ job/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-'''
-django model field:
-- html widget
-- validation
-- db size
-'''
-
 JOB_TYPE = (
     ('Full Time', 'full time'),
     ('Part Time', 'Part time'),
@@ -24,6 +17,14 @@ class Job(models.Model):   # table
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
