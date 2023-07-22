1e1955ce5c028140707e07f6bca93fd9da60ed24
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-07-22 22:54:59 +0300

    confirm sqllight db

--- job/models.py
+++ job/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+django model field:
+- html widget
+- validation
+- db size
+'''
+
+
+class Job(models.Model):   # table
+    title = models.CharField(max_length=10)  # column
