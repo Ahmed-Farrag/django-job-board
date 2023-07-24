6a253d2e542bd2cce0985aac29e5ff1e0d1c7a22
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-07-24 17:09:31 +0300

    forms

--- job/models.py
+++ job/models.py
@@ -40,3 +40,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Apply(models.Model):
+    # realtion for job
+    job = models.ForeignKey(
+        'Job', related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
